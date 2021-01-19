f0fcec1f8b64e0918b72bd0bea9680e514c0715b
Jan Nicklas <j.nicklas@me.com>
2021-01-19 19:35:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,99 @@
+<div align="center">
+  <h1>useRefEffect</h1>
+  <br/>
+  An `useEffect` hook optimized for `useRef`.
+  <br/>
+    <a href="https://www.npmjs.com/package/react-use-ref-effect">
+      <img src="https://img.shields.io/npm/v/react-use-ref-effect.svg?style=flat-square" />
+    </a>
+    <a href='https://github.com/jantimon/react-use-ref-effect/workflows/CI?query=workflow%3A"CI"'>
+       <img alt="Github Actions" src="https://github.com/jantimon/react-use-ref-effect/workflows/CI/badge.svg?style=flat-square">
+    </a>
+    <a href="https://bundlephobia.com/result?p=react-use-ref-effect">
+      <img src="https://img.shields.io/bundlephobia/minzip/react-use-ref-effect.svg" alt="bundle size">
+    </a> 
+</div>
+
+---
+
+Executes an effect directly after React attaches a ref to a DOM node.  
+Allows cleaning up once React detaches the DOM node from the ref again.
+  
+- The hook does __not__ triggering additional renderings.
+- The hook size is __only 200b__.
+
+# API
+
+## useCallbackRef API
+Use case: every time you have to react to ref change
+
+- `const ref = useRefEffect(callback)` - would call provided `callback` when ref is changed.
+
+- `const ref = useCallbackRef(callback, [])` - would call provided `callback` when ref is changed or a dependency is changed - similar to useEffect.
+
+- `const ref = useCallbackRef(() => { return cleanupCallback }, [])` - would call provided `cleanUpcallback` once the component unmounts or if react removes the referenced DOM element 
+
+```js
+import { useRefEffect } from 'react-use-ref-effect';
+
+const Component = () => {
+
+  const ref = useRefEffect((element) => {
+    console.log('Element', element, 'is now available');
+    return () => {
+      console.log('Element', element, 'is no longer available');
+    }
+  }, []);
+
+  return <div ref={ref}>Hello World</div>
+}
+```
+
+# Motivation
+
+React is delivering two powerful hooks `useRef` and `useEffect` however they don't work properly in combination:
+
+```js
+const ref = useRef();
+useEffect(() => {
+  // do sth with ref.current
+}, [ref.current])
+```
+
+✅ &nbsp; doesn't trigger additional renderings on mount
+
+🚫 &nbsp; fails to execute the effect for conditionally rendered components  
+(e.g. `isOpen && <span ref={ref}>Demo</span>`)
+
+🚫 &nbsp; fails to execute the effect for lazy rendered components  
+(e.g. `<LazyComponent><span ref={ref}>Demo</span></LazyComponent>`)
+
+🚫 &nbsp; fails to execute the effect if a child controls the time to mount  
+(e.g. `<Slider waitFor={3000}><span ref={ref}>Demo</span></Slider>`)
+
+<br /><br />
+
+By using a pattern from the official [react hooks faq](https://reactjs.org/docs/hooks-faq.html#how-can-i-measure-a-dom-node) `useRefEffect` can be used as a safe replacement:
+
+```js
+const ref = useRefEffect((current) => {
+  // do sth with current
+}, [])
+```
+✅ &nbsp; doesn't trigger additional renderings on mount
+
+✅ &nbsp; executes effect and effect cleanup for conditionally and lazy rendered components  
+(e.g. `isOpen && <span ref={ref}>Demo</span>`)
+
+✅ &nbsp; executes effect and effect cleanup for lazy rendered components  
+(e.g. `<LazyComponent><span ref={ref}>Demo</span></LazyComponent>`)
+
+✅ &nbsp; executes effect and effect cleanup if a child controls the time to mount  
+(e.g. `<Slider waitFor={3000}><span ref={ref}>Demo</span></Slider>`)
+
+# Similar packages:
+- [use-callback-ref](https://github.com/theKashey/use-callback-ref) - great utils around refs and callbacks
+
+# License
+MIT
+
---
+++ test/index.test.tsx
@@ -0,0 +1,247 @@
+import * as React from 'react';
+import { useState } from 'react';
+import * as ReactDOM from 'react-dom';
+import { useRefEffect } from '../src';
+import { act, Simulate } from 'react-dom/test-utils';
+
+describe('it', () => {
+  it('executes effect on render', () => {
+    const div = document.createElement('div');
+    let effectRuns = 0;
+    const Demo = () => {
+      const ref = useRefEffect(() => {
+        effectRuns++;
+      }, []);
+      return <div ref={ref}>Demo</div>;
+    };
+    expect(effectRuns).toBe(0);
+    ReactDOM.render(<Demo />, div);
+    ReactDOM.unmountComponentAtNode(div);
+    expect(effectRuns).toBe(1);
+  });
+
+  it('passes in the html element', () => {
+    const div = document.createElement('div');
+    let effectElement: any = null;
+    const Demo = () => {
+      const ref = useRefEffect(element => {
+        effectElement = element;
+      }, []);
+      return (
+        <a ref={ref} href="/">
+          Demo
+        </a>
+      );
+    };
+    ReactDOM.render(<Demo />, div);
+    ReactDOM.unmountComponentAtNode(div);
+    expect(effectElement instanceof HTMLAnchorElement).toBe(true);
+  });
+
+  it('executes cleanup on unmount', () => {
+    const div = document.createElement('div');
+    let cleanupRuns = 0;
+    const Demo = () => {
+      const ref = useRefEffect(() => {
+        return () => {
+          cleanupRuns++;
+        };
+      }, []);
+      return <div ref={ref}>Demo</div>;
+    };
+    ReactDOM.render(<Demo />, div);
+    expect(cleanupRuns).toBe(0);
+    ReactDOM.unmountComponentAtNode(div);
+    expect(cleanupRuns).toBe(1);
+  });
+
+  it('executes cleanup on unmount', () => {
+    const div = document.createElement('div');
+    let cleanupRuns = 0;
+    const Demo = () => {
+      const ref = useRefEffect(() => {
+        return () => {
+          cleanupRuns++;
+        };
+      }, []);
+      return <div ref={ref}>Demo</div>;
+    };
+    ReactDOM.render(<Demo />, div);
+    expect(cleanupRuns).toBe(0);
+    ReactDOM.render(<React.Fragment />, div);
+    ReactDOM.unmountComponentAtNode(div);
+    expect(cleanupRuns).toBe(1);
+  });
+
+  it('executes effect on dependency change', () => {
+    const div = document.createElement('div');
+    let effectRuns = 0;
+    let cleanupRuns = 0;
+    const Demo = () => {
+      const [counter, setCounter] = useState(0);
+      const ref = useRefEffect(() => {
+        effectRuns++;
+        return () => {
+          cleanupRuns++;
+        };
+      }, [counter]);
+      return (
+        <button ref={ref} onClick={() => setCounter(counter + 1)}>
+          Demo
+        </button>
+      );
+    };
+    ReactDOM.render(<Demo />, div);
+    expect(effectRuns).toBe(1);
+    expect(cleanupRuns).toBe(0);
+    act(() => {
+      Simulate.click(div.querySelector<HTMLButtonElement>('button')!);
+    });
+    expect(effectRuns).toBe(2);
+    expect(cleanupRuns).toBe(1);
+    ReactDOM.unmountComponentAtNode(div);
+    expect(effectRuns).toBe(2);
+    expect(cleanupRuns).toBe(2);
+  });
+
+  it('executes effect on dom change', () => {
+    const div = document.createElement('div');
+    let effectRuns = 0;
+    let cleanupRuns = 0;
+    let element: any = {};
+    const Demo = () => {
+      const [counter, setCounter] = useState(0);
+      const ref = useRefEffect(newElement => {
+        element = newElement;
+        effectRuns++;
+        return () => {
+          cleanupRuns++;
+        };
+      }, []);
+      return (
+        <>
+          {counter > 0 && <span ref={ref}>{counter}</span>}
+          <button onClick={() => setCounter(counter + 1)}>Demo</button>
+        </>
+      );
+    };
+    ReactDOM.render(<Demo />, div);
+    expect(effectRuns).toBe(0);
+    expect(cleanupRuns).toBe(0);
+    act(() => {
+      Simulate.click(div.querySelector<HTMLButtonElement>('button')!);
+    });
+    expect(effectRuns).toBe(1);
+    expect(cleanupRuns).toBe(0);
+    expect(element.tagName).toBe('SPAN');
+    ReactDOM.unmountComponentAtNode(div);
+    expect(effectRuns).toBe(1);
+    expect(cleanupRuns).toBe(1);
+  });
+
+  it('executes effect on dom or on dependency change', () => {
+    const div = document.createElement('div');
+    let effectRuns = 0;
+    let cleanupRuns = 0;
+    let elementName = '';
+    const Demo = () => {
+      const [counter, setCounter] = useState(0);
+      const ref = useRefEffect<HTMLElement>(newElement => {
+        elementName = newElement.tagName;
+        effectRuns++;
+        return () => {
+          elementName = '';
+          cleanupRuns++;
+        };
+      }, [counter]);
+      return (
+        <>
+          {counter > 0 && <span ref={ref}>{counter}</span>}
+          <button onClick={() => setCounter(counter + 1)}>Demo</button>
+        </>
+      );
+    };
+    ReactDOM.render(<Demo />, div);
+    expect(effectRuns).toBe(0);
+    expect(cleanupRuns).toBe(0);
+    console.log('click');
+    act(() => {
+      Simulate.click(div.querySelector<HTMLButtonElement>('button')!);
+    });
+    expect(effectRuns).toBe(1);
+    expect(elementName).toBe('SPAN');
+    expect(cleanupRuns).toBe(0);
+    act(() => {
+      Simulate.click(div.querySelector<HTMLButtonElement>('button')!);
+    });
+    expect(effectRuns).toBe(2);
+    expect(cleanupRuns).toBe(1);
+    ReactDOM.unmountComponentAtNode(div);
+    expect(effectRuns).toBe(2);
+    expect(cleanupRuns).toBe(2);
+  });
+
+  it('executes effect on ref target change', () => {
+    const div = document.createElement('div');
+    let elementName: string = '';
+    const Demo = () => {
+      const [counter, setCounter] = useState(0);
+      const ref = useRefEffect<HTMLElement>(newElement => {
+        elementName = newElement.tagName;
+        return () => {
+          elementName = '';
+        };
+      }, []);
+      return (
+        <>
+          {counter === 0 ? (
+            <h1 ref={ref}>Click the button</h1>
+          ) : (
+            <span ref={ref}>{counter}</span>
+          )}
+          <button onClick={() => setCounter(counter + 1)}>Demo</button>
+        </>
+      );
+    };
+    expect(elementName).toBe('');
+    ReactDOM.render(<Demo />, div);
+    expect(elementName).toBe('H1');
+    act(() => {
+      Simulate.click(div.querySelector<HTMLButtonElement>('button')!);
+    });
+    expect(elementName).toBe('SPAN');
+    ReactDOM.unmountComponentAtNode(div);
+    expect(elementName).toBe('');
+  });
+
+  it('executes effect on manual setting', () => {
+    const div = document.createElement('div');
+    let effectRuns = 0;
+    let cleanupRuns = 0;
+    const Demo = () => {
+      const ref = useRefEffect(() => {
+        effectRuns++;
+        return () => {
+          cleanupRuns++;
+        };
+      }, []);
+      return (
+        <button onClick={() => { ref.current = div }}>
+          Demo
+        </button>
+      );
+    };
+    ReactDOM.render(<Demo />, div);
+    expect(effectRuns).toBe(0);
+    expect(cleanupRuns).toBe(0);
+    act(() => {
+      Simulate.click(div.querySelector<HTMLButtonElement>('button')!);
+    });
+    expect(effectRuns).toBe(1);
+    expect(cleanupRuns).toBe(0);
+    ReactDOM.render(<React.Fragment />, div)
+    ReactDOM.unmountComponentAtNode(div);
+    expect(effectRuns).toBe(1);
+    expect(cleanupRuns).toBe(1);
+  });
+});
